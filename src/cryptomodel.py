cba8566fa2ede61294f1ad9df465dc45cec50818
Harrison Floam <harrisonfloam@gmail.com>
2023-04-23 15:14:43 -0400

    Change crypto_lstm.py to cryptolstm.py, update CryptoModel

--- src/cryptomodel.py
+++ src/cryptomodel.py
@@ -8,11 +8,12 @@ class CryptoModel:
     
     ### Parameters:
     -----------
-    - model_class: class
+    - model_class_name: string
         The trading model class to be used (e.g. CryptoLSTM).
     - **kwargs:
         Additional arguments to be passed to the model class constructor.
     """
-    def __init__(self, model_class, **kwargs):
-        super().__init__()
+    def __init__(self, model_class_name, **kwargs):
+        model_module = __import__('models.' + model_class_name.lower(), fromlist=[model_class_name])
+        model_class = getattr(model_module, model_class_name)
         self.model = model_class(**kwargs)
