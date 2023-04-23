f1dd5b580752ff7168da1688e1d09c9c58b57b97
Harrison Floam <harrisonfloam@gmail.com>
2023-04-23 15:24:25 -0400

    Add cryptotrader_testcases.py to hold unit test cases

---
+++ test/cryptotrader_testcases.py
@@ -0,0 +1,11 @@
+## Test Cases for CryptoTrader
+# Harrison Floam, 23 April 2023
+
+# Import Libaries and Modules
+import unittest
+
+from src.cryptotrader import CryptoModel
+from .testcryptotrader import TestCryptoTrader
+
+class CryptoTraderTestCase(unittest.TestCase):
+    
