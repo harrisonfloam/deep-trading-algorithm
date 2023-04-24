511f27df2ee87b19c12d80eb728577e4bfd6c69e
Harrison Floam <harrisonfloam@gmail.com>
2023-04-23 22:40:17 -0400

    Fix MACD

--- src/cryptotrader.py
+++ src/cryptotrader.py
@@ -113,7 +113,8 @@ class CryptoTrader:
         # Compute the indicators
         data['sma'] = ta.sma(data['close'], length=20)                            # Simple Moving Average
         data['rsi'] = ta.rsi(data['close'], length=14)                            # Relative Strength Index
-        data['macd'] = ta.macd(data['close'], fast=12, slow=26, signal=9)['histogram']   # Moving Average  Convergence Divergence   #FIXME: MACD isn't working
+        macd = ta.macd(data['close'], fast=12, slow=26, signal=9)    # MACD
+        data['macd'] = macd['MACD_12_26_9']
 
         data.dropna(inplace=True)  # Drop rows with NaN values
         data.reset_index(drop=True, inplace=True)  # Reset the index
