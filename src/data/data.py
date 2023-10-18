9c1d7c776c73c3798a1c349b78af2b7483ab71a9
Harrison Floam <harrisonfloam@gmail.com>
2023-10-18 19:11:58 -0400

    refactor DataProcessor, add DataBundle class, add to data/utils.py

--- src/data/data.py
+++ src/data/data.py
@@ -15,86 +15,76 @@ from sklearn.linear_model import LinearRegression
 
 # Import modules
 from src.utils import get_project_root
+from src.data.utils import forward_backward_fill, to_datetime_index, train_test_val_split, scale_data, unscale_data
 
+
+class DataBundle:
+    def __init__(self, data=None, train=None, test=None, val=None, scaler=None):
+        self.data = data
+        self.train = train
+        self.test = test
+        self.val = val
+        self.scaler = scaler
+    
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            if value is not None:
+                setattr(self, key, value)
+                
+    def get_bundle(self):
+        return self.train, self.test, self.val
 
 class DataProcessor:
-    def __init__(self, filename="bitstampUSD_1-min_data_2012-01-01_to_2021-03-31.csv", start='2021-03-27 15:04:00', end='2021-03-31 00:00:00', verbose=False):
-        self.filename = filename
-        self.path = os.path.join(get_project_root(), 'data', filename)    # Filepath of data
-        self.start = start
-        self.end = end
+    def __init__(self, start, end, verbose=False):
+        self.start = pd.to_datetime(start)
+        self.end = pd.to_datetime(end)
         self.verbose = verbose
 
-    def load_data(self):
-        # Import Data
-        self.data = pd.read_csv(self.path)
+    def load_data(self, filename):
+        self.path = os.path.join(get_project_root(), 'data', filename)    # Filepath of data
+        data = pd.read_csv(self.path)
+        databundle = DataBundle(data=data)
         if self.verbose:
-            print(f'Data loaded --------- data/{self.filename}')
+            print(f'Data loaded --------- data/{filename}')
+        return databundle
 
-    def clean_data(self):
-        # Clean data
-        self.data.fillna(method="ffill", inplace=True)  # Forward/backward fill prices
-        self.data.fillna(method="bfill", inplace=True)
+    def clean_data(self, databundle):
+        data = to_datetime_index(databundle.data, 'Timestamp') # Convert to datetime index
+        data = forward_backward_fill(data)
+        data.sort_values('Timestamp', inplace=True) # Sort the dataframe by timestamp
+        databundle.update(data=data)    # Update data
 
-    def add_features(self):
-        self.data['SMA'] = ta.sma(close=self.data['Close'], length=20)    # Simple Moving Average
-        self.data['RSI'] = ta.rsi(close=self.data['Close'], length=14)    # Relative Strength Index
-        macd = ta.macd(close=self.data['Close'], fast=12, slow=26, signal=9)  # MACD
-        self.data['MACD'] = macd['MACD_12_26_9']
+    def add_features(self, databundle):
+        data = databundle.data  # Extract data from bundle
+        data['SMA'] = ta.sma(close=data['Close'], length=20)    # Simple Moving Average
+        data['RSI'] = ta.rsi(close=data['Close'], length=14)    # Relative Strength Index
+        macd = ta.macd(close=data['Close'], fast=12, slow=26, signal=9)  # MACD
+        data['MACD'] = macd['MACD_12_26_9']
         #self.data['bb_upper'], self.data['bb_middle'], self.data['bb_lower'] = ta.bbands(close=self.data['Close'], length=20) # Bollinger Bands
         #self.data['VWAP'] = ta.vwap(high=self.data['High'], low=self.data['Low'], close=self.data['Close'], volume=self.data['Volume_(BTC)'])
+        data['log_ret'] = ta.log_return(close=data['Close'], cumulative=False)    # Log returns
+        data['percent_ret'] = ta.percent_return(close=data['Close'], cumulative=False)*10    # Percent returns
+        data.dropna(inplace=True)  # Drop rows with NaN values
+        
+        databundle.update(data=data)    # Update data
 
-        self.data['log_ret'] = ta.log_return(close=self.data['Close'], cumulative=False)    # Log returns
-        self.data['percent_ret'] = ta.percent_return(close=self.data['Close'], cumulative=False)*10    # Percent returns
-
-        self.data.dropna(inplace=True)  # Drop rows with NaN values
-        # self.data.reset_index(drop=True, inplace=True)  # Reset the index
-
-    def trim_data(self):
-        # Convert to datetime index
-        self.data['Timestamp'] = pd.to_datetime(self.data['Timestamp'], unit='s') # Convert the Timestamp column to datetime
-        self.data.set_index('Timestamp', inplace=True)
-
-        # Convert start and end to datetime
-        start = pd.to_datetime(self.start)
-        end = pd.to_datetime(self.end)
-
-        # Select rows between start and end dates
-        self.data = self.data.loc[start:end]
-
+    def trim_data(self, databundle):
+        data = databundle.data  # Extract data from bundle
+        data = data.loc[self.start:self.end]    # Select rows between start and end dates
         if self.verbose:
-            print(f'Using data from {start} to {end} with {len(self.data)-1} rows and {len(self.data.columns)} columns')
+            print(f'Using data from {self.start} to {self.end} with {len(data)-1} rows and {len(data.columns)} columns')
+        databundle.update(data=data)    # Update data
 
-    def split_data(self):
-        # Split data
-        self.data.sort_values('Timestamp', inplace=True) # Sort the dataframe by timestamp
-        train_val, self.test = train_test_split(self.data, test_size=0.15, shuffle=False) # Split the data
-        self.train, self.val = train_test_split(train_val, test_size=0.176, shuffle=False)  # Create validation set from train
+    def split_data(self, databundle):
+        train_test_val_split(input=databundle, test_percent=0.15, val_percent=0.15)
 
-    def scale_data(self, exclude_columns): 
-        for col in self.data.columns:
-            if col in exclude_columns:
-                continue
-            if col == 'percent_ret':
-                self.percent_ret_scaler = StandardScaler()
-                scaler = self.percent_ret_scaler
-            else:
-                scaler = MinMaxScaler(feature_range=(0, 1))
-            self.train[col] = scaler.fit_transform(self.train[col].values.reshape(-1, 1))
-            self.val[col] = scaler.transform(self.val[col].values.reshape(-1, 1))
-            self.test[col] = scaler.transform(self.test[col].values.reshape(-1, 1))
+    def scale_data(self, databundle, exclude_columns): 
+        scale_data(databundle, exclude_columns)
 
-    def unscale_data(self, scaled_df):
-        if self.percent_ret_scaler is None:
-            raise ValueError("The 'percent_ret' column has not been scaled yet.")
-        
-        scaled_percent_ret = scaled_df['percent_ret'].values
-        unscaled_percent_ret = self.percent_ret_scaler.inverse_transform(scaled_percent_ret.reshape(-1, 1))
-        
-        unscaled_df = scaled_df.copy()
-        unscaled_df['percent_ret'] = unscaled_percent_ret.flatten()
+    def unscale_data(self, scaled_df, databundle):
+        #TODO: make sure we can access databundle later...
+        unscaled_df = unscale_data(scaled_df=scaled_df, databundle=databundle)
         return unscaled_df
-
     
     def normalize_with_rolling_stats(self, window_size):
         for col in self.data.columns:
@@ -103,25 +93,29 @@ class DataProcessor:
             self.data[col] = (self.data[col] - rolling_mean) / rolling_std
         self.data.dropna(inplace=True)
         
-    def detrend_data(self, exclude_columns):
+    def detrend_data(self, databundle, exclude_columns):
+        train, test, val = databundle.get_bundle()
         for col in self.data.columns:
             if col in exclude_columns:
                 continue
             linreg = LinearRegression()
-            x = np.arange(len(self.data)).reshape(-1, 1)
-            y = self.data[col].values
-            linreg.fit(x, y)
-            trend = linreg.predict(x)
-            self.data[col] = y - trend
+            for subset in [train, test, val]:
+                x = np.arange(len(subset)).reshape(-1, 1)
+                y = subset[col].values
+                if subset == train:
+                    linreg.fit(x, y)
+                trend = linreg.predict(x)
+                subset[col] = subset[col].values - trend
+        databundle.update(train=train, test=test, val=val)
 
-    def prepare_data(self, exclude_columns_detrend, exclude_columns_scale):
-        self.load_data()
-        self.clean_data()
-        self.add_features()
-        self.trim_data()
-        self.split_data()
-        self.detrend_data(exclude_columns=exclude_columns_detrend)
-        self.scale_data(exclude_columns=exclude_columns_scale)
+    def prepare_data(self, filename, exclude_columns_detrend, exclude_columns_scale):
+        databundle = self.load_data(filename)
+        self.clean_data(databundle)
+        self.add_features(databundle)
+        self.trim_data(databundle)
+        self.split_data(databundle)
+        self.detrend_data(databundle=databundle, exclude_columns=exclude_columns_detrend)
+        self.scale_data(databundle=databundle, exclude_columns=exclude_columns_scale)
         
     def create_dataloaders(self, window, batch_size, exclude_input_columns):
         train_loader = TimeSeriesDataLoader(df=self.train, window=window, 
